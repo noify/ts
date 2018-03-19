a09a4de79f26b03b31e54224e52974f42ab54d49
nano <shinanome@qq.com>
2018-03-19 16:03:49 +0800

    update

--- README.md
+++ README.md
@@ -13,3 +13,77 @@ tsc -w
 ```
 
 ## 基础类型
+
+声明变量时必须要定义数据类型
+
+```ts
+// 基本数据类型
+let bool: boolean = false;
+let num: number = 6;
+let str: string = "bob";
+let u: undefined = undefined;
+let n: null = null;
+
+// any 和 Object 类似，都允许你给它赋任意值；但 Object 不能够在它上面调用任意的方法，即便它真的有这些方法
+let obj: Object = {};
+let any: any = {};
+
+// 数组
+let arr: Array<any> = [];
+let list: number[] = [1, 2, 3];
+let x: [string, number] = ['hello', 10];
+x[3] = 'world'; // 联合类型
+
+// 其他
+let date: Date = new Date();
+let fun: Function = function (){};
+```
+
+枚举类型
+
+```ts
+enum Color {Red = 0, Green, Blue}
+let c: Color = Color.Green;
+
+let colorName: string = Color[2]; // 赋值'Blue'因为上面代码里它的值是2
+```
+
+void 和 never
+
+ - void： 没有任何类型，无返回值
+ - never：永不存在的值的类型
+
+```ts
+function warnUser(): void {
+  alert("This is my warning message");
+}
+
+let unusable: void = undefined;
+unusable = null;
+
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+  throw new Error(message);
+}
+
+// 推断的返回值类型为never
+function fail() {
+  return error("Something failed");
+}
+
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+  while (true) {
+  }
+}
+```
+
+断言，只在编译阶段起作用
+
+```ts
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length;
+
+strLength = (someValue as string).length;
+```
--- js/basic-types.js
+++ js/basic-types.js
@@ -1,21 +1,16 @@
 var bool = false;
 var num = 6;
 var str = "bob";
-var fun = function () { };
 var u = undefined;
 var n = null;
-var date = new Date();
 var obj = {};
 var any = {};
 var arr = [];
-var arr2 = [];
 var list = [1, 2, 3];
-var list2 = [1, 2, 3];
-var x;
-x = ['hello', 10];
-console.log(x[0].substr(1));
+var x = ['hello', 10];
 x[3] = 'world';
-console.log(x[5].toString());
+var date = new Date();
+var fun = function () { };
 var Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
@@ -23,34 +18,12 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 var c = Color.Green;
-var Color2;
-(function (Color2) {
-    Color2[Color2["Red"] = 1] = "Red";
-    Color2[Color2["Green"] = 2] = "Green";
-    Color2[Color2["Blue"] = 3] = "Blue";
-})(Color2 || (Color2 = {}));
-var c2 = Color2.Green;
-var Color3;
-(function (Color3) {
-    Color3[Color3["Red"] = 1] = "Red";
-    Color3[Color3["Green"] = 2] = "Green";
-    Color3[Color3["Blue"] = 4] = "Blue";
-})(Color3 || (Color3 = {}));
-var c3 = Color3.Green;
 var colorName = Color[2];
-alert(colorName);
-var notSure = 4;
-notSure = "maybe a string instead";
-notSure = false;
-notSure.ifItExists();
-notSure.toFixed();
-var prettySure = 4;
-var list4 = [1, true, "free"];
-list4[1] = 100;
 function warnUser() {
     alert("This is my warning message");
 }
 var unusable = undefined;
+unusable = null;
 function error(message) {
     throw new Error(message);
 }
@@ -63,6 +36,5 @@ function infiniteLoop() {
 }
 var someValue = "this is a string";
 var strLength = someValue.length;
-var someValue2 = "this is a string";
-var strLength2 = someValue.length;
+strLength = someValue.length;
 //# sourceMappingURL=basic-types.js.map
